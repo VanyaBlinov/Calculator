257c3cb3167355fda0c78c821bcee382c855a0cf
Vanya Blinov <iv-blin@yandex.ru>
2023-03-13 21:15:36 +0700

    Created the Layout, add functions for buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var textScreen = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            onClickNumberListener(but1, 1)
+            onClickNumberListener(but2, 2)
+            onClickNumberListener(but3, 3)
+            onClickNumberListener(but4, 4)
+            onClickNumberListener(but5, 5)
+            onClickNumberListener(but6, 6)
+            onClickNumberListener(but7, 7)
+            onClickNumberListener(but8, 8)
+            onClickNumberListener(but9, 9)
+            onClickNumberListener(butZero, 0)
+        }
+
+    }
+
+    private fun addNumberOnScreen(number: Int) {
+        textScreen += number.toString()
+    }
+
+    private fun printUpdateScreen() {
+        binding.tvScreen.text = textScreen
+    }
+
+    private fun onClickNumberListener(idNumber: Button, number: Int) {
+        idNumber.setOnClickListener {
+            addNumberOnScreen(number)
+            printUpdateScreen()
+        }
     }
 }
